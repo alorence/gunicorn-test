3e435ee81d8849e46bce8494c72d616b71fa42cc
Antoine Lorence <antoine.lorence@gmail.com>
2018-06-30 14:35:18 +0200

    Update information

--- main/views.py
+++ main/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render, render_to_response
+import os
+
+import django
+import gunicorn
+from django.conf import settings
+from django.shortcuts import render_to_response
+
+import main
 
 
 # Create your views here.
-def home(request, *args, **kwargs):
 
-    return render_to_response("main/home.html")
+
+
+def home(request, *args, **kwargs):
+    nginx_conf_file = os.path.join(settings.BASE_DIR, "nginx_config", "gunicorn_https.conf")
+    with open(nginx_conf_file, "r")as f:
+        conf_content = f.read()
+
+    context = {
+        "app_version": main.__version__,
+        "gun_version": gunicorn.__version__,
+        "dj_version": django.__version__,
+        "request": request,
+        "url_scheme": request.environ['wsgi.url_scheme'],
+        "nginx_conf": conf_content,
+    }
+
+    return render_to_response("main/home.html", context)
